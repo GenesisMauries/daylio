69af22bee86ec86cc33dfbe8a6de34620ef29cee
Génesis Mauries <exodo_54@hotmail.com>
2023-08-23 17:36:25 -0600

    add header modularization

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,24 +1,6 @@
-import { useEffect } from "react";
-import {UserAuth} from "../context/AuthContext";
-import {useNavigate} from "react-router-dom";
-import { Button, Container, Typography, Avatar, Grid } from "@mui/material";
-import GoogleIcon from "@mui/icons-material/Google";
-
+import { Container } from "@mui/material";
+import Header from "../components/Header";
 function Login() {
-  const navigate = useNavigate();
-  const {user,googleSignIn} = UserAuth();
-  const signIn=async()=>{
-    try {
-      await googleSignIn();
-    } catch (error) {
-      console.log(error);
-    }
-  }
-  useEffect(()=>{
-if(user!=null){
-  navigate("/")
-}
-  },[navigate, user])
   return (
     <Container
       sx={{
@@ -29,38 +11,7 @@ if(user!=null){
         minHeight: "100vh", // Establece la altura mínima de la ventana para centrar verticalmente
       }}
     >
-      <Grid container columns={12} justifyContent="center" alignItems="center">
-        <Grid item xs={10} sm={5} md={3}>
-          <Typography variant="h1">Daylio</Typography>
-        </Grid>
-        <Grid item xs={2} sm={2} md={2}>
-          <Avatar
-            src="https://github.com/GenesisMauries/lux/blob/master/src/images/icono-45-lux.png?raw=true"
-            alt="Logo"
-            variant="square"
-            sx={{ width: 75, height: 75 }}
-          ></Avatar>
-        </Grid>
-      </Grid>
-      <Grid
-        container
-        columns={12}
-        justifyContent="center"
-        alignItems="center"
-        sx={{ mt: 3 }}
-      >
-        <Grid item>
-          <Button
-            variant="contained"
-            color="inherit"
-            size="large"
-            startIcon={<GoogleIcon />}
-            onClick={signIn} 
-          >
-            Entrar con Google
-          </Button>
-        </Grid>
-      </Grid>
+      <Header />
     </Container>
   );
 }
